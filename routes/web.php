3eca4eba3f51257ace88286dbf0d085ac462977d
omericoramos <omericoramos@gmail.com>
2024-10-25 18:04:00 -0300

    consumindo api github e guardando dados no banco

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\PullResquestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', PullResquestController::class)->name('home');
