61f7df2236f41f786e2fc81d6c1eed6eceb7b7af
omericoramos <omericoramos@gmail.com>
2024-11-22 11:08:18 -0300

    adicionando comando para executar o job PullRequestsListSyncJob

--- app/Service/Github/Console/Commands/PullRequestSyncCommand.php
+++ app/Service/Github/Console/Commands/PullRequestSyncCommand.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Service\Github\Console\Commands;
 
 use App\Service\Github\Job\PullRequestsListSyncJob;
 use Illuminate\Console\Command;
 
 class PullRequestSyncCommand extends Command
 {
-    protected $signature = 'app:pull-request-sync-command {repositoryFullName}';
+    protected $signature = 'github:pull-request-sync-command {repositoryFullName}';
 
     protected $description = 'Comando para executar os jobs de sincronização da listagem de pull requests do repositório';
 
