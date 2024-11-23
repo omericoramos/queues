e039d2b297582bb5f0dada042e591bef23cb5015
omericoramos <omericoramos@gmail.com>
2024-11-23 17:33:57 -0300

    refatorando codigo query string url

--- app/Service/Github/PullRequestService.php
+++ app/Service/Github/PullRequestService.php
@@ -8,13 +8,18 @@ class PullRequestService
     public function __construct(
         private ClientService $clientService
     ) {}
-    
+
     // obtem uma lista de pull requests de um repositorio do github, passando o nome do repositorio e a pagina
     public function getListPullRequests(string $repositoryFullName, ?int $pageNumber = 1): array
     {
+        $queryString = http_build_query([
+            'page' => $pageNumber,
+            'state' => 'all'
+        ], arg_separator:'&', encoding_type: PHP_QUERY_RFC3986);
+
         $pullRequests = $this->clientService
             ->getClient()
-            ->get("/repos/{$repositoryFullName}/pulls?state=all&page={$pageNumber}")
+            ->get("/repos/{$repositoryFullName}/pulls?{$queryString}")
             ->json();
         return $pullRequests;
     }
